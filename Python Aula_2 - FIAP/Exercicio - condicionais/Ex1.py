013ea9ae6b376728777f493bacef8dc47ab6d270
joaomorangoni <joaopedromorangoni@gmail.com>
2026-03-05 11:43:27 -0300

    inicio dos desafios

---
+++ Python Aula_2 - FIAP/Exercicio - condicionais/Ex1.py
@@ -0,0 +1,8 @@
+num = float(input("Digite um número: "))
+
+if num > 0:
+    print("Seu número é positivo")
+elif num < 0:
+    print("Seu número é negivo")
+else: 
+    print("Seu número é 0")
---
+++ Python Aula_2 - FIAP/Exercicio - condicionais/Ex4.py
@@ -0,0 +1,9 @@
+letra = input("Insira uma letra: ")
+vogal = ["a", "e", "i", "o", "u"]
+
+if letra == 
+    print("Sua letra é uma vogal")
+else:
+    print("Sua letra é uma consoante")
+
+# Não finalizado ainda
