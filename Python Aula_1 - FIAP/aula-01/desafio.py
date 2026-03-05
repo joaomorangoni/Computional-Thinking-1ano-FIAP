48c2304808970d3d05c49f95c5f9573da573d06b
joaomorangoni <joaopedromorangoni@gmail.com>
2026-03-05 10:43:45 -0300

    Organizando as pastas

--- Python Aula_1 - FIAP/aula-01/desafio.py
+++
@@ -1,7 +0,0 @@
-caixaA = 10
-caixaB = 20
-
-caixaA, caixaB = caixaB, caixaA
-
-print(f"CaixaA = ", caixaA)
-print(f"CaixaB = ", caixaB)
---
+++ Python Aula_1 - FIAP/desafio.py
@@ -0,0 +1,7 @@
+caixaA = 10
+caixaB = 20
+
+caixaA, caixaB = caixaB, caixaA
+
+print(f"CaixaA = ", caixaA)
+print(f"CaixaB = ", caixaB)
