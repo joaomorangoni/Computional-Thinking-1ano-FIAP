fe8fba8a56d194fbd60140a721ea7054369415b3
João Pedro Morangoni <124791845+joaomorangoni@users.noreply.github.com>
2026-03-18 11:51:58 -0300

    Add files via upload

---
+++ Aula 04/Ex1.py
@@ -0,0 +1,9 @@
+matriz = [
+    [2, 6, 8],
+    [1, 9, 9],
+    [3, 5, 7]
+]
+
+soma = sum(matriz[0]) + sum(matriz[1]) + sum(matriz[2])
+
+print(soma)
---
+++ Aula 04/Ex2.py
@@ -0,0 +1,8 @@
+matriz = [
+    [6,2],
+    [9,5]
+]
+escalar = 2
+resultado = [[l * escalar for l in c] for c in matriz]
+
+print(resultado)
